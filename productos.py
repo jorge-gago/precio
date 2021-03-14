20590a656177aeea2e0ba7ed56f82f26fea96b07
jorge gago <jorgegago2526@gmail.com>
2021-03-14 13:30:26 +0100

    ajuste

--- productos.py
+++ productos.py
@@ -1,60 +1,75 @@
-from urllib import request as url_req
 import requests as req
-
-#buscar en pagina
-nombre_page = '<span id="productTitle" class="a-size-large product-title-word-break">'
-pre_temp = 'id="newBuyBoxPrice" class="a-size-base a-color-price header-price a-text-normal"'
-pic = 'id="imgTagWrapperId"'
+import extrac
+import datetime
+import base
 
 hdr = { 'User-Agent' : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36"}
 
-def llamadas_url(url = None, headers = hdr):
-    urls = req.get( url, headers = hdr )
 
+def llamadas_url(url , headers = hdr):   
+    urls = req.get( url, headers = hdr )
     data = urls.text
+
     return data
 
-def precio(page):
-    tramo = buscar(page, pre_temp)
-    fin = (buscar(page, "€", tramo)) + tramo + 1
-    ini = (buscar(page, ">", tramo)) + tramo + 1
-    precio = page[ ini: fin]
+class producto():
 
-    return precio
+    def __init__(self):
+        self.nombre_pro = ""
+        self.url_producto = ""
+        self.data_url = ""
+        self.nombre = ""
+        self.precio = "00.00"
+        self.foto = ""
+        self.deseado = "00.00"
+        self.pre_min = "--.--"
+        self.pre_max = "--.--"
+        self.precios = [[ 0, 0]]
+        self.page = ""
 
-def nombre(page):
-    nombre_p = buscar( page, nombre_page)
-    nombre_ini = buscar(page, ">" , nombre_p) + nombre_p + 1
-    nombre_f = buscar(page,'<' , nombre_ini) + nombre_ini 
-    nombre_producto =(page[ nombre_ini: nombre_f]).replace("\n","")
+    def change(self): # llama los datos de base y los pasa a object
+        pass
 
-    return  nombre_producto
+    def  urls(self, url):
+        self.url_producto = url
+        self.data_url = llamadas_url(self.url_producto)
+        self.page = extrac.web( url)
 
-def foto(page):
-    fot = buscar( page, pic)
-    ini = buscar( page, "data-old-hires=", fot) + fot + len("data-old-hires='")
-    fin = buscar( page, ".jpg", ini) + ini + 4
-    fotos = page[ ini: fin]
+    def  precio_page(self):
+        self.precio = extrac.precio(self.data_url, self.page).replace(".", ",")
+        self.add_pre()
 
-    return fotos
+    def add_pre(self):
+        fecha = datetime.date.today().strftime("%d/%m/%y")
+        c = self.precio.find(",")+3
+        precio_act = [ self.precio[:c], fecha ]
 
-def buscar( data = " ", obj = "", posi = 0):
-    buscar = data[ posi:].find(obj)
-    return buscar
+        if precio_act == self.precios[0]:
+            pass
+        else:
+            self.precios.insert(0 , precio_act)
+        if len(self.precios) > 10: 
+            self.precios.pop() 
+      
+        print(self.precios)
+
+    def  nombre_page(self):
+        self.nombre = extrac.nombre(self.data_url, self.page).strip()
+
+    def foto_page(self):
+        foto_url = extrac.foto(self.data_url, self.page)[1:]
+        #foto = pillow.imagen.open(foto_url, headers = hdr)
+        #self.foto = foto
+
+    def al (self):
+        return self.page, self.url_producto, self.nombre, self.precio, self.deseado, self.pre_min, self.pre_max, self.precios, self.foto
 
 
 
+    
 
-def pruebas(t2):
 
-    print("nombre")
-    print(nombre(t2))
-
-    print("precio")
-    print(precio(t2))
-
-    print("foto")
-    print(foto(t2))
+    
 
 
     
@@ -62,6 +77,16 @@ def pruebas(t2):
 # para prueba
 if __name__ == "__main__":
 
+    prueba = producto()
+    prueba.urls('https://www.amazon.es/Moulinex-Multimoulinette-Compact-DJ300110-capacidad/dp/B00I96MAXC/ref=sr_1_50?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=moulinex&qid=1610394421&refinements=p_89%3AMoulinex&rnid=1692911031&s=kitchen&sr=1-50&th=1')
+    
+    prueba.precio_page()
+    prueba.precio_page()
+    prueba.nombre_page()
+
+    for i in prueba.al():
+        print(i)
+"""
     url = 'https://www.amazon.es/Moulinex-Multimoulinette-Compact-DJ300110-capacidad/dp/B00I96MAXC/ref=sr_1_50?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=moulinex&qid=1610394421&refinements=p_89%3AMoulinex&rnid=1692911031&s=kitchen&sr=1-50&th=1'
 
     pruebas(llamadas_url( url))
@@ -83,6 +108,7 @@ if __name__ == "__main__":
     url = 'https://www.amazon.es/Controlador-PowerA-licencia-oficial-Windows/dp/B07NPY1YT5/ref=sr_1_20?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&dchild=1&keywords=mando%2Bxbox&qid=1613386373&s=videogames&sr=1-20&th=1'
 
     pruebas(llamadas_url( url))
+"""
     
 
  
